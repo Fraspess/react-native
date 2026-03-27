625c6e740964d9719b4e73c9ade4e5b32933ed4c
CODER <CODER@rivne.itstep.org>
2026-03-27 17:50:36 +0200

    saloooo

--- backend/PD421_WEB_API/WEB_API/Controllers/Auth/AuthController.cs
+++ backend/PD421_WEB_API/WEB_API/Controllers/Auth/AuthController.cs
@@ -1,34 +1,63 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WEB_API.BLL.Constants;
 using WEB_API.BLL.Dtos.Auth;
 using WEB_API.BLL.Services.Auth;
+using WEB_API.BLL.Services.Storage;
 using WEB_API.DAL.Entities.Identity;
 
-namespace WEB_API.Controllers.Auth
+namespace WEB_API.Controllers.Auth;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class AuthController(UserManager<UserEntity> userManager,
+    IJWTTokenService tokenService, RoleManager<RoleEntity> roleManager,
+    IStorageService storage) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuthController(UserManager<UserEntity> userManager,
-    IJWTTokenService tokenService) : ControllerBase
+    [HttpPost]
+    public async Task<IActionResult> Login([FromBody] LoginDTO model)
     {
-        [HttpPost]
-        public async Task<IActionResult> Login([FromBody] LoginDTO model)
+        var user = await userManager.FindByEmailAsync(model.Email);
+
+        if (user == null)
+            return Ok(new TokenDTO());
+
+        var isValidPassword = await userManager
+            .CheckPasswordAsync(user, model.Password);
+        if (!isValidPassword)
+            return Ok(new TokenDTO());
+
+        user.IsDeleted = false;
+        await userManager.UpdateAsync(user);
+        var result = await tokenService.CreateTokenAsync(user);
+
+        return Ok(result);
+    }
+    [HttpPost]
+    public async Task<IActionResult> Register([FromForm] RegisterDto model)
+    {
+        var user = new UserEntity()
         {
-            var user = await userManager.FindByEmailAsync(model.Email);
-
-            if (user == null)
-                return Ok(new TokenDTO());
-
-            var isValidPassword = await userManager
-                .CheckPasswordAsync(user, model.Password);
-            if (!isValidPassword)
-                return Ok(new TokenDTO());
-
-            user.IsDeleted = false;
-            await userManager.UpdateAsync(user);
-            var result = await tokenService.CreateTokenAsync(user);
-
-            return Ok(result);
+            Email = model.Email,
+            FirstName = model.FirstName,
+            LastName = model.LastName,
+            Image = await storage.SaveImageAsync(model.ImageFile),
+            UserName = model.Email
+        };
+        var result = await userManager.CreateAsync(user, model.Password);
+        if (result.Succeeded)
+        {
+            await userManager.AddToRoleAsync(user, Roles.User);
+            var tokenInfo = await tokenService.CreateTokenAsync(user);
+            return Ok(tokenInfo);
         }
+        else
+        {
+            Console.WriteLine("------ ERROR WHEN CREATING USER: ");
+            return BadRequest(result.Errors);
+        }
+
+
+
     }
 }
